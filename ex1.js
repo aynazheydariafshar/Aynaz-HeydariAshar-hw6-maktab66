0d1bc0456df374b3bb6502c8370a22e0f2ee3652
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-13 12:30:27 +0330

    bulid array char from input two and check all of char and add one to count

--- ex1.js
+++ ex1.js
@@ -1,5 +1,14 @@
 function solution(inputone , inputtwo){
-    
+    let count = 0
+    const chartwo = inputtwo.split("")
+    for(let i of inputone){
+      let element = chartwo.findIndex(index => index === i)
+      if(element >= 0){
+        count++
+        chartwo.splice(element, 1)
+      }
+    }
+    return count
 }
 
-console.log(solution(aabcc,adcaa))
+console.log(solution("aabcc","adcaa"))
