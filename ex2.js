c074c07db7cd3980810b542bb303d43bb3bb6055
Aynaz HeydariAfshar <aynazhadriafshar@gmail.com>
2021-12-13 14:27:54 +0330

    build algoritm and return input from solution function

--- ex2.js
+++ ex2.js
@@ -1,5 +1,24 @@
 function solution(input){
-    
+    const res = []
+    for(let i=0 ; i<input.length ; i++){
+        if(input[i] === -1){
+            res.push(i) //find index of -1
+        }
+    }
+
+    input.sort(function(a, b) {
+        return a - b; //sort array
+    })
+
+    for(let k=0 ; k<res.length ; k++){
+        input.splice(0,k) //delete -1 from array
+    }
+
+    for(let j of res){
+        input.splice(j, 0, -1) //add -1 to array
+    }
+
+    return input
 
 }
 console.log(solution([-1, 150, 190, 170, -1, -1, 160, 180]))
